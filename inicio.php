181ba3babda22455e1b5eaef8dc532cd748568e4
Diego-Paul123 <garciaubaldodiegopaul@gmail.com>
2026-05-12 17:44:25 -0600

    Ya esta todo lo visual y acomodado lo de admin falta maestors y alumnos

--- inicio.php
+++ inicio.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="visual_login.css">
 </head>
 
 <body>
